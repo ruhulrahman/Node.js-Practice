7f16a8e3bfdcd558b49fe9ecde9c2e67cdd5650e
ruhulrahman <ruhulrahman2233@gmail.com>
2021-04-11 23:24:26 +0600

    index.js. udpated

--- fsModule/index.js
+++ fsModule/index.js
@@ -2,11 +2,17 @@ var fs = require('fs')
 var http = require('http')
 var server = http.createServer(function (req, res) {
     if (req.url==="/") {
-        fs.readFile('home.html', function (error, data) {
-            res.writeHead(200, { 'Content-Type':'text/html' })
-            res.write(data)
-            res.end()
-        })
+        //Asyncrhonus data reader
+        // fs.readFile('home.html', function (error, data) {
+        //     res.writeHead(200, { 'Content-Type':'text/html' })
+        //     res.write(data)
+        //     res.end()
+        // })
+
+        let data = fs.readFileSync('home.html')
+        res.writeHead(200, { 'Content-Type':'text/html' })
+        res.write(data)
+        res.end()
     }
 })
 server.listen(3000)
